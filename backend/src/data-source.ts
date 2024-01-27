a8bd146b14d409927182c8bf1f57f14c84c61cef
vinicius-soares-dev <vinicius.soares.dev>
2024-01-27 17:52:39 -0300

    chore: update entities

---
+++ backend/src/data-source.ts
@@ -0,0 +1,15 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { Users } from "./entity/Users";
+
+export const AppDataSource = new DataSource({
+  type: "postgres",
+  host: "localhost",
+  port: 5432,
+  username: "postgres",
+  password: "root",
+  database: "article",
+  synchronize: true,
+  logging: false,
+  entities: [Users],
+})
