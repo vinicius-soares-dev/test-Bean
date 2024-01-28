3ad103a11e66ea4171df36f9e75c6abfa7b7ca94
vinicius-soares-dev <vinicius.soares.dev>
2024-01-28 14:22:26 -0300

    fix:  fix login error

--- backend/src/controllers/Login.ts
+++ backend/src/controllers/Login.ts
@@ -3,13 +3,19 @@ import { Users } from "../entity/Users";
 import { AppDataSource } from "../data-source";
 import jwt from "jsonwebtoken";
 
+const SECRET_KEY = 'JDSOFJG';
+
 export const Login = async (req: Request, res: Response) => {
   const { username, password } = req.body;
-  const credentialsDatabase = AppDataSource.manager.getRepository(Users);
-  const existingUser = await credentialsDatabase.findOne({ where: { username } && { password }});
+
+  const userRepository = AppDataSource.manager.getRepository(Users);
+  const existingUser = await userRepository.findOne({ where: { username, password} });
+
   if (!existingUser) return res.status(401).send("Invalid Credentials");
-  const token = jwt.sign({ userId: existingUser.id}, 'JDSOFJG', {
-    expiresIn: 300 * 300
+
+  const token = jwt.sign({ userId: existingUser.id}, SECRET_KEY, {
+    expiresIn: 3600  // 1 hour
   });
+  
   return res.json({ auth: true, token: token }); 
 }
